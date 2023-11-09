a183f75ff4790e5d77b8a284320acfde96afb40a
kdd9512 <hyunwoo8805@gmail.com>
2023-11-09 23:55:56 +0900

    20231109 commit
    REST 클라이언트 학습용 프로젝트 초기생성

---
+++ src/main/java/com/kdd9512/springbootrestcontroller/SimpleController.java
@@ -0,0 +1,22 @@
+package com.kdd9512.springbootrestcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String hello() throws InterruptedException{
+        Thread.sleep(5000l);  // 5 second
+        return "hello";
+    }
+
+    @GetMapping("/world")
+    public String world() throws InterruptedException{
+        Thread.sleep(3000l);  // 5 second
+        return "world";
+    }
+
+
+}
---
+++ src/test/java/com/kdd9512/springbootrestcontroller/SpringbootrestcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdd9512.springbootrestcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestcontrollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
