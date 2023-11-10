1a877e5e2549378fbbb3c7496b99131363528154
kdd9512 <hyunwoo8805@gmail.com>
2023-11-10 23:58:43 +0900

    20231110 commit
    REST 컨트롤러 학습

---
+++ src/main/java/com/kdd9512/springbootrestcontroller/RestRunner.java
@@ -0,0 +1,13 @@
+package com.kdd9512.springbootrestcontroller;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
